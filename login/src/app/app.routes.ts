348e3f2822a001c7fb2e2d9bd95d88834077b11c
sonal <sonal.shinde@starfish.digital>
2024-09-06 20:06:21 +0530

    v1-login-application-creation

---
+++ login/src/app/app.routes.ts
@@ -0,0 +1,22 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+export const routes: Routes = [
+    {
+        path: '', redirectTo: 'login', pathMatch: 'full'
+    },
+    {
+        path: 'login', component: LoginComponent
+    },
+    {
+        path: 'login', component: LayoutComponent,
+        children: [
+            {
+                path: 'dashboard',
+                component: DashboardComponent
+            }
+        ]
+    },
+];
